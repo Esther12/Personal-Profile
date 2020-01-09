e9c9001ead1257d2636131b8be8f934c3349b2cf
Yating(Esther) Su <yaringsu@foxmail.com>
2020-01-09 17:46:53 -0500

    Fixed mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-$("#project1").click(function() {
+$("#project").on("click", ".card", function() {
+  $(".card-body").toggle();
   $(".card-info").css({
     display: "block",
     "transition-timing-function": "ease-out",
     color: "#fff"
+  });
+  $(".card-body").css({
+    background: "rgba(65, 63, 63, 0.9)"
   });
 });
